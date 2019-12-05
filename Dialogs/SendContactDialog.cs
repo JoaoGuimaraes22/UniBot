cb3055d517518979d0e32523aa403d28169b94b4
Joao Guimaraes <Joao.Guimaraes@ctr.unipartner.com>
2019-12-05 18:42:59 +0000

    Trying to put email sending into bot

--- Dialogs/SendContactDialog.cs
+++ Dialogs/SendContactDialog.cs
@@ -11,17 +11,27 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Bot.Schema;
 using UniBotJG.CognitiveModels;
 using UniBotJG.StateManagement;
+using MailKit;
+using System.Management.Automation.Runspaces;
+using System.ComponentModel;
+using MailKit.Net.Smtp;
+using MimeKit;
+using System.Data;
+using System.Drawing;
+using System.Text;
 
 
 namespace UniBotJG.Dialogs
 {
     public class SendContactDialog : ComponentDialog
     {
+
         private readonly LuisSetup _recognizer;
         protected readonly ILogger Logger;
         private readonly UserState _userState;
 
-        public SendContactDialog(LuisSetup luisRecognizer, ILogger<SendContactDialog> logger, UserState userState/*, GetHelpDialog getHelp*/)
+
+        public SendContactDialog(LuisSetup luisRecognizer, ILogger<SendContactDialog> logger, UserState userState)
             : base(nameof(SendContactDialog))
         {
             _recognizer = luisRecognizer;
@@ -43,6 +53,7 @@ namespace UniBotJG.Dialogs
 
         private async Task<DialogTurnResult> SendAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
+          
             var userProfile = new UserProfile();
             if (userProfile.ChosePhone == true)
             {
@@ -51,9 +62,31 @@ namespace UniBotJG.Dialogs
             }
             else
             {
+                /*
                 //Send email
-                return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = MessageFactory.Text("Thank You. More information on the Special Account for emigrants was sent to your email adress. Is there anything helse I can help you with?") }, cancellationToken);
+                //Send email
+                SmtpClient client = new SmtpClient();
+                client.Timeout = 10000;
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
 
+                BodyBuilder builder = new BodyBuilder();
+                builder.HtmlBody = "TestHtmlBody";
+
+                //Define the mail headers
+                MimeMessage mail = new MimeMessage();
+                mail.Subject = "TestSubject";
+                mail.Body = builder.ToMessageBody();
+                mail.From.Add(new MailboxAddress("sebastiao.guimaraes22@gmail.com"));
+                mail.To.Add(new MailboxAddress("sebastiao.guimaraes22@gmail.com"));
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                client.Connect("smtp.gmail.com", 465, false);
+                client.Authenticate("sebastiao.guimaraes22@gmail.com", "tiaotiao22");
+                client.Send(mail);
+                client.Disconnect(true);
+                */
+
+                return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = MessageFactory.Text("Thank You. More information on the Special Account for emigrants was sent to your email adress. Is there anything helse I can help you with?") }, cancellationToken);
+                
             }
 
         }
